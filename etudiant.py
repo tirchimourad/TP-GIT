b525f6dd2d1418f2463b069347c3e4f76b6dc6c5
Mourad Tirchi <tirchi.mourad@gmail.com>
2025-04-18 20:26:00 +0100

    Autre modification

--- etudiant.py
+++ etudiant.py
@@ -1,5 +1,7 @@
 def ajouter_etudiant(nom):
     print(f"étudiant {nom} ajouté avec succès.")
+    print("ajout de l'étudiant")
+    return
 
 def ajouter_note(nom,note):
     print(f"{nom} a obtenu une note de {note}.")    
